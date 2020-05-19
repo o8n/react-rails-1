e32c2be8cb30410160bea528645a6e3aa9cf1e25
okamotchan <m.okamotchan@gmail.com>
2020-05-19 21:53:21 +0900

    first commit

---
+++ server/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root 'home#index'
+  get 'home/index'
+
+  namespace 'api', {format: 'json'} do
+    namespace 'v1' do
+      resources :tasks
+    end
+  end
+end
